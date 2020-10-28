e6c0ec726d3d4efb092c2b86bdf5a05575620d90
jsan <ahsan.wtc@gmail.com>
2020-10-28 10:57:27 +0100

    initial release

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
 function App() {
+  library.add(fab, faCheckSquare, faCoffee);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+
+          <h2>I'm Ahsan J. Butt</h2>
+          
+          <div className="social-container">
+            <a href="https://www.linkedin.com/in/ahjaved/" target="_blank" className="App-link" rel="noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+            <a href="https://github.com/ahsanwtc" target="_blank" className="App-link" rel="noreferrer"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+            <a href="https://twitter.com/jsantv" target="_blank" className="App-link" rel="noreferrer"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>            
+          </div>
+        </header>
+        
+        
+      </div>
+    </React.Fragment>
   );
 }
 
